b7a9611f85b3026a7224ba16bceebfe6cadc6038
j000 <jarymut+github@gmail.com>
2018-03-14 22:47:09 +0100

    [Zestaw1][Zad3] Dodaj procinfo.c i procinfo.h

---
+++ Zestaw1/procinfo.c
@@ -0,0 +1,19 @@
+#define _XOPEN_SOURCE 700
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int procinfo(const char* name) {
+	printf(
+		"%-10s: pid:%6d | ppid:%5d | uid:%6d | gid:%6d | pgid:%5d\n",
+		name,
+		(int) getpid(),
+		(int) getppid(),
+		(int) getuid(),
+		(int) getgid(),
+		(int) getpgid(0)
+	);
+	return 0;
+}
