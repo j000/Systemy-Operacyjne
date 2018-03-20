10a087ab84a12f4e0af995e0ff3822cdc28d4957
j000 <jarymut+github@gmail.com>
2018-03-20 10:33:57 +0100

    [Zestaw1][Zad5] Dodaj drzewko1.c i drzewko2.c

---
+++ Zestaw1/drzewko2.c
@@ -0,0 +1,34 @@
+#define _XOPEN_SOURCE 700 /* getpgid() */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "procinfo.h"
+
+#define N 3
+
+#define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
+
+int main(int UNUSED(argc), char **argv) {
+	procinfo(argv[0]);
+
+	for (int n = 0; n < N; ++n) {
+		switch(fork()) {
+		case -1:
+			/* błąd */
+			perror("fork error");
+			exit(EXIT_FAILURE);
+		case 0:
+			/* potomny */
+			procinfo(argv[0]);
+			break;
+		default:
+			/* macierzysty */
+			n=N; /* wyjdź z pętli */
+			wait(NULL);
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
